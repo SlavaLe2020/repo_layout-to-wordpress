9861f40bb596b0a1195ffbbcff724005cdc941da
SlavaLe2020 <levchonokslava@gmail.com>
2022-02-10 20:19:47 +0200

    add wordpress 10

--- index.php
+++ index.php
@@ -15,92 +15,46 @@
             </div>
             <div class="project">
                 <div class="project__title">
-                    ПРОЕКТЫ ДОМОВ ИЗ БРУСА
+                    <?php the_field('project__title') ?>
                 </div>
-                <div class="project__item">
 
                     <?php
-$posts = get_field('project__item');
 
-if( $posts ): ?>
-    <ul>
-    <?php foreach( $posts as $post ): ?>
+                     $posts = get_field('project__item');
 
-        // Setup this post for WP functions (variable must be named $post).
-        <?php setup_postdata($post); ?>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <span>A custom field from this post: <?php the_field( 'author' ); ?></span>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-    <?php wp_reset_postdata(); ?>
-<?php endif; ?>
-                    <!-- <div class="project__name">
-                        ДОМ #1
-                    </div>
-                    <div class="project__size">
-                        Размер дома: 6 x 6
-                    </div>
-                    <div class="project__area">
-                        Общая площадь дома: 54 кв. м
-                    </div>
-                    <div class="project__price">
-                        Стоимость - 402 000 рублей
-                    </div>
-                    <div class="project__images">
-                        <div class="project__images-item">
-                            <img src="img/project-1.jpg" alt="">
-                        </div>
-                        <div class="project__images-item">
-                            <img src="img/project-2.jpg" alt="">
-                        </div>
-                    </div> -->
-                </div>
-                <!-- <div class="project__item">
-                    <div class="project__name">
-                        ДОМ #1
-                    </div>
-                    <div class="project__size">
-                        Размер дома: 6 x 6
-                    </div>
-                    <div class="project__area">
-                        Общая площадь дома: 54 кв. м
-                    </div>
-                    <div class="project__price">
-                        Стоимость - 402 000 рублей
-                    </div>
-                    <div class="project__images">
-                        <div class="project__images-item">
-                            <img src="img/project-1.jpg" alt="">
-                        </div>
-                        <div class="project__images-item">
-                            <img src="img/project-2.jpg" alt="">
-                        </div>
-                    </div>
-                </div> -->
-                <!-- <div class="project__item">
-                    <div class="project__name">
-                        ДОМ #1
-                    </div>
-                    <div class="project__size">
-                        Размер дома: 6 x 6
-                    </div>
-                    <div class="project__area">
-                        Общая площадь дома: 54 кв. м
-                    </div>
-                    <div class="project__price">
-                        Стоимость - 402 000 рублей
-                    </div>
-                    <div class="project__images">
-                        <div class="project__images-item">
-                            <img src="img/project-1.jpg" alt="">
-                        </div>
-                        <div class="project__images-item">
-                            <img src="img/project-2.jpg" alt="">
-                        </div>
-                    </div>
-                </div> -->
+                    if( $posts ): ?>
+                        <ul>
+                        <?php foreach( $posts as $post ): ?>
+
+                            <?php setup_postdata($post); ?>
+                            <li class="project__item">
+                                
+                                <div class="project__name">
+                                    <?php the_field('project__name') ?>
+                                </div>
+                                <div class="project__size">
+                                    <?php the_field('project__size') ?>
+                                </div>
+                                <div class="project__area">
+                                    <?php the_field('project__area') ?>
+                                </div>
+                                <div class="project__price">
+                                    <?php the_field('project__price') ?>
+                                
+                                </div>
+                                <div class="project__images">
+                                    <div class="project__images-item">
+                                        <img src="<?php the_field('project__images-item-1') ?>" alt="">
+                                    </div>
+                                    <div class="project__images-item">
+                                        <img src="<?php the_field('project__images-item-2') ?>" alt="">
+                                    </div>
+                                </div> 
+                            </li>
+                        <?php endforeach; ?>
+                        </ul>
+                        <?php wp_reset_postdata(); ?>
+                    <?php endif; ?>
             </div>
 
         </div>
